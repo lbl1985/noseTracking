a98f176dd5119433f03c0885695c46b1909d7210
lbl1985-office <herbert19lee@gmail.com>
2011-07-04 06:48:42 -0400

    debug for blobs

--- mysuperdemo.cpp
+++ mysuperdemo.cpp
@@ -18,7 +18,7 @@
 #include "ObjectDetector.h"
 
 #include "BlobResult.h"
-#include "blobs.h"
+//#include "blobs.h"
 
 #include <QApplication>
 #include <QMetaType>
@@ -756,12 +756,12 @@ void thresholdSegmentation(Rect r, ntk::RGBDImage* current_frame, Mat& dst){
 	bool iswrite;
 	const int nchannel = 1;
 	vector<Rect> faces;
-	iswrite = imwrite("faceROI.png", outFrameROI);
+	iswrite = imwrite("faceROI.png", maskROI);
 	//iswrite = cvSaveImage("faceROI.jpeg", pOutFrame, &nchannel);
 
 	// ---- blob segmentation on maskROI by using cvblobslib ----
 	// ---		Third Trial	---
-	visualizeBlobs("faceROI.png", "faceRIO");
+	//visualizeBlobs("faceROI.png", "faceRIO");
 
 
 
@@ -774,30 +774,33 @@ void thresholdSegmentation(Rect r, ntk::RGBDImage* current_frame, Mat& dst){
 	//cvThreshold(pImgMaskROIThr, pImgMaskROIThr, 0.1, 255, CV_THRESH_BINARY_INV);
 
 	// ---		Second Trial	---
-	//IplImage* original = cvLoadImage("faceROI.png", 0);
-	//IplImage* originalThr = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U, 1);
-	//IplImage* displayBiggestBlob = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U, 1);
-	//CBlobResult blobs;
-	//CBlob biggestBlob;
-	////IplImage source = maskROIThr;	IplImage* pSource = &source;
-	//blobs =  CBlobResult( originalThr, NULL, 1 );;
-	//blobs.GetNthBlob(CBlobGetArea(), 0, biggestBlob);
-	//biggestBlob.FillBlob(displayBiggestBlob, CV_RGB(255, 0, 0));
+	IplImage* original = cvLoadImage("faceROI.png", 0);
+	IplImage* originalThr = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U, 1);
+	IplImage* displayBiggestBlob = cvCreateImage(cvGetSize(original), IPL_DEPTH_8U, 3);
+	CBlobResult blobs;
+	CBlob biggestBlob;
+	//IplImage source = maskROIThr;	IplImage* pSource = &source;
+	//blobs = CBlobResult(
+	cvThreshold(original, originalThr, 0.1, 255, CV_THRESH_BINARY_INV);
+	blobs =  CBlobResult( originalThr, NULL, 255);
+	printf("%d blobs \n", blobs.GetNumBlobs());
+	blobs.GetNthBlob(CBlobGetArea(), 0, biggestBlob);
+	biggestBlob.FillBlob(displayBiggestBlob, CV_RGB(255, 0, 0));
 
-	//// Drawing the eclipse and Rect on the blob
-	//Mat mat(displayBiggestBlob);
+	// Drawing the eclipse and Rect on the blob
+	Mat mat(displayBiggestBlob);
 
-	//cv::RotatedRect blobEllipseContour;
-	//cv::Rect blobRectContour;
-	////RotatedRect blobEllipseContour;
-	//blobEllipseContour = biggestBlob.GetEllipse();
-	//blobRectContour = biggestBlob.GetBoundingBox();
-	////cv::ellipse(
-	//cv::ellipse(mat, blobEllipseContour, cv::Scalar(0,255, 0), 3, CV_AA);
-	//cv::rectangle(mat, blobRectContour, cv::Scalar(255, 0, 0), 3, CV_AA);
-	////cv::ellipse(mat, blobEllipseContour);
-	//
-	//cv::imshow("faceRIO", mat);
+	cv::RotatedRect blobEllipseContour;
+	cv::Rect blobRectContour;
+	//RotatedRect blobEllipseContour;
+	blobEllipseContour = biggestBlob.GetEllipse();
+	blobRectContour = biggestBlob.GetBoundingBox();
+	//cv::ellipse(
+	cv::ellipse(mat, blobEllipseContour, cv::Scalar(0,255, 0), 3, CV_AA);
+	cv::rectangle(mat, blobRectContour, cv::Scalar(255, 0, 0), 3, CV_AA);
+	//cv::ellipse(mat, blobEllipseContour);
+	
+	cv::imshow("faceRIO", mat);
 
 	
 

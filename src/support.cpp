ead8cc3436c8ab5d878880ae521a2b32807219fa
lbl1985-office <herbert19lee@gmail.com>
2011-07-07 22:12:59 -0400

    .h file for all the supporting functions. For now, only pointDistance has been added. This is good for better code structure

---
+++ src/support.cpp
@@ -0,0 +1,11 @@
+#include "support.h"
+
+double pointDistance(Point tempPoint, Point nosePoint)
+{
+	double diff, xdiff, ydiff;
+	xdiff = tempPoint.x - nosePoint.x;
+	ydiff = tempPoint.y - nosePoint.y;
+	diff = sqrt(xdiff * xdiff + ydiff * ydiff);	
+	printf("diff is: %f \n", diff);
+	return (diff);
+}
---
+++ src/support.h
@@ -0,0 +1,5 @@
+#include <ntk/utils/opencv_utils.h>
+
+using namespace cv;
+
+double pointDistance(Point a, Point b);
